52945e6e224924e05f6e046b18a0d2d7d44c9863
MdSikder <mdporagbsmrstu@gmail.com>
2023-03-26 10:10:17 +0600

    update

--- ddt/data.py
+++ ddt/data.py
@@ -2,7 +2,6 @@
 import csv
 
 import pandas as pd
-import xlsxwriter
 
 import openpyxl
 
@@ -10,7 +9,7 @@ import openpyxl
 l = "'"
 k = "'"
 str1 = ", SqlType.text()"
-dataframe1 = pd.read_excel('my.xlsx')
+dataframe1 = pd.read_excel('my1.xlsx')
 
 # workbook = xlsxwriter.Workbook('ty.xlsx')
 #
